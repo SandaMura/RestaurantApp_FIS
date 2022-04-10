e359d8dabe7c29186e5688a47d05ad82d99161cb
SandaMura <sanda.mura@student.upt.ro>
2022-04-10 16:29:40 +0300

    New Project

---
+++ FIS_Restaurant/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.fis_restaurant {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.fis_restaurant to javafx.fxml;
+    exports com.example.fis_restaurant;
+}
