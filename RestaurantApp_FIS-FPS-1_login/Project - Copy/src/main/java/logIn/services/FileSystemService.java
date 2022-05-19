c230eb5b5dc6bb3d8a4501836a6ff938dc029e0a
SandaMura <sanda.mura@student.upt.ro>
2022-05-19 23:14:12 +0300

    19 mai- am scris testele pentru LogIn, inca sunt probleme la run test

--- RestaurantApp_FIS-FPS-1_login/Project - Copy/src/main/java/logIn/services/FileSystemService.java
+++ RestaurantApp_FIS-FPS-1_login/Project - Copy/src/main/java/logIn/services/FileSystemService.java
@@ -4,11 +4,20 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileSystemService {
-    private static final String APPLICATION_FOLDER = "database";
+
+    public static String APPLICATION_FOLDER = "database";
     private static final String USER_FOLDER = "";
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
+
+    //public static String APPLICATION_FOLDER = ".registration-example";
 
     public static Path getPathToFile(String... path) {
         return APPLICATION_HOME_PATH.resolve(Paths.get("", path));
     }
+
+    public static Path getApplicationHomeFolder() {
+        return Paths.get(USER_FOLDER, APPLICATION_FOLDER);
+    }
+
+
 }
--- RestaurantApp_FIS-FPS-1_login/Project - Copy/src/test/java/firstTest/TestingTests.java
+++ RestaurantApp_FIS-FPS-1_login/Project - Copy/src/test/java/firstTest/TestingTests.java
@@ -1,12 +1,59 @@
 package firstTest;
 
 import com.example.project.TestingCalculation;
+/*
+import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.io.;
+
+import org.testfx.api.FxAssert;
+
+import org.testfx.matcher.control.LabeledMatchers;
+
+import org.testfx.assertions.api.Assertions;*/
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+//import org.loose.fis.sre.services.FileSystemService;
+//import org.loose.fis.sre.services.UserService;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import static org.testfx.assertions.api.Assertions.assertThat;
 
 
 
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.*;
+//import org.loose.fis.sre.exceptions.UsernameAlreadyExistsException;
+//mport org.loose.fis.sre.model.User;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.testfx.assertions.api.Assertions.assertThat;
 public class TestingTests{
 
 
@@ -17,7 +64,7 @@ private final TestingCalculation calculator = new TestingCalculation();
 public void testAddition(){
         int actualResult = calculator.add(1, 1);
 
-        Assertions.assertEquals(2, actualResult);
+        Assertions.assertThat(2).isEqualTo(actualResult);
 
         }
 
