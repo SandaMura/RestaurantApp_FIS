f63a7138e0712d53b6c2ed049104acde39e81e80
luyza <luyza1032002@gmail.com>
2022-05-23 12:11:03 +0300

    delete review

--- RestaurantApp_FIS-FPS-1_login/Project - Copy/src/main/java/Review/controllers/DisplayReviewController.java
+++ RestaurantApp_FIS-FPS-1_login/Project - Copy/src/main/java/Review/controllers/DisplayReviewController.java
@@ -6,11 +6,14 @@ import Review.Review;
 import Review.services.ReviewService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import org.dizitart.no2.objects.Cursor;
+
+import static logIn.services.DBUtils.AlertBox;
 
 
 public class DisplayReviewController {
@@ -22,7 +25,7 @@ public class DisplayReviewController {
     private TableColumn<Review, String> Name;
 
     @FXML
-    private TableView<Review> table2;
+    private static TableView<Review> table2;
 
     @FXML public void initialize(){
         Name.setCellValueFactory(new PropertyValueFactory<Review, String>("Username"));
@@ -34,5 +37,18 @@ public class DisplayReviewController {
             products.add(f);
         }
         table2.setItems(products);
+    }
+    public static ObservableList<String> Rv=FXCollections.observableArrayList();
+        public static void deleteR() {
+        Review review=table2.getSelectionModel().getSelectedItem();
+        if(review!=null) {
+            Rv.remove(review.getUsername());
+            AlertBox("Review deleted");
+        }
+        else
+            AlertBox("Please select a review");
+    }
+    public static ObservableList<String> getNewList(){
+        return Rv;
     }
 }
