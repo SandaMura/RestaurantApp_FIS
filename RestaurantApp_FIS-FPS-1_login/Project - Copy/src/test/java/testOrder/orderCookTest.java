57015d93a586f9699d55ac2525e6ca8e6c2f278b
SandaMura <sanda.mura@student.upt.ro>
2022-05-24 10:59:30 +0300

    Interface tests for Orders (FxRobot)

--- RestaurantApp_FIS-FPS-1_login/Project - Copy/src/test/java/testOrder/orderCookTest.java
+++ RestaurantApp_FIS-FPS-1_login/Project - Copy/src/test/java/testOrder/orderCookTest.java
@@ -1,4 +1,53 @@
 package testOrder;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import logIn.services.FileSystemService;
+import logIn.services.UserService;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+
+import Admin.AdminView.*;
+
+import static org.assertj.core.api.InstanceOfAssertFactories.type;
+import static org.testfx.assertions.api.Assertions.assertThat;
+
+
+@ExtendWith(ApplicationExtension.class)
 public class orderCookTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before Class");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After Class");
+    }
+
+    @Start
+    void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Cook_view.fxml"));
+        primaryStage.setTitle("Customer Example");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    @Test
+    void testViewOrders(FxRobot robot) {
+        robot.clickOn("#ViewOrders");
+        //  robot.clickOn("#BackToCustomer");
+        robot.clickOn("#listOrder");
+
+    }
+
 }
