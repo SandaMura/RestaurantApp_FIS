ca6d07b584accba00b31e31562d61f40be630122
luyza <luyza1032002@gmail.com>
2022-05-25 22:06:26 +0300

    FPS-113 Display review for admin

--- src/main/java/Review/controllers/DisplayReviewForAdminController.java
+++ src/main/java/Review/controllers/DisplayReviewForAdminController.java
@@ -1,14 +1,23 @@
 package Review.controllers;
 
+import App.RestaurantApplication;
 import MenuForm.controllers.DisplayMenuController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 import static logIn.services.DBUtils.AlertBox;
 
 public class DisplayReviewForAdminController {
-   
+   @FXML
+   private Button backAdmin;
     @FXML
     public ListView<String> listRv = new ListView<>();
 
@@ -29,6 +38,19 @@ public class DisplayReviewForAdminController {
     }
 
 
+    public void backToAdmin(ActionEvent actionEvent) {
+            Stage stage;
+            Parent root;
+            stage = (Stage) backAdmin.getScene().getWindow();
+            try {
+                root = FXMLLoader.load(RestaurantApplication.class.getClassLoader().getResource("Admin_view.fxml"));
+                stage.setScene(new Scene(root, 600, 450));
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            
+        }
+    }
 }
 
 
